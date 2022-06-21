04ad1b2e20edbdd79ac55e1437f3dd534cca29f0
misostack <misostack.com@gmail.com>
2022-06-21 18:08:09 +0700

    Init

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ["./src/*.html", "./src/**/*.html", "./docs/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1498d5",
+      },
+      fontFamily: {
+        body: ["arial"],
+        heading: ["'Merriweather', serif"],
+      },
+      textColor: {
+        primary: "#1498d5",
+      },
+      backgroundColor: {
+        primary: "#1498d5",
+        body: "#f1f1f1",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
